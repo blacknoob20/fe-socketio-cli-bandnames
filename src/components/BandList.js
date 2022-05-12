d19d9836cfdb9ccbac688ea1c090c9913f28d70d
Cristhian Guerrero <cristian.guerrero@guayas.gob.ec>
2022-05-11 21:48:13 -0500

    Band Names | Incrementar los votos utilizando sockets | Version 1.1.0

--- src/components/BandList.js
+++ src/components/BandList.js
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 
-export const BandList = () => {
+export const BandList = ({ data, votar }) => {
+    const [bands, setBands] = useState(data);
+
+    useEffect(() => {
+        setBands(data);
+
+    }, [data]);
+
+    const nameChanged = (event, id) => {
+        const newName = event.target.value;
+        setBands(
+            bands => bands.map(band => {
+                if (band.id === id) band.name = newName;
+                return band;
+            })
+        );
+    }
+
+    const onLeaveFocus = (id, name) => {
+        console.log(id, name);
+
+        //TODO: Disparar el socket
+    }
+
     const createRows = () => {
         return (
-            <tr>
-                <td>
-                    <button className='btn btn-primary'> +1 </button>
-                </td>
-                <td>
-                    <input
-                        className='form-control'
-                    />
-                </td>
-                <td><h3> 15 </h3></td>
-                <td>
-                    <button className='btn btn-danger'>Borrar</button>
-                </td>
-            </tr>
+            bands.map(band => (
+                <tr key={band.id}>
+                    <td>
+                        <button className='btn btn-primary' onClick={() => votar(band.id)}> +1 </button>
+                    </td>
+                    <td>
+                        <input
+                            className='form-control'
+                            value={band.name}
+                            onChange={event => nameChanged(event, band.id)}
+                            onBlur={() => onLeaveFocus(band.id, band.name)}
+                        />
+                    </td>
+                    <td><h3> {band.votes} </h3></td>
+                    <td>
+                        <button className='btn btn-danger'>Borrar</button>
+                    </td>
+                </tr>
+            ))
         );
     }
     return (
